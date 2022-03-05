27148c821e99e951ae4d7589785bb1e0027b59f8
amanukyan11 <Aram.Manukyan11@gmail.com>
2022-03-04 17:38:26 -0800

    initial commit

--- app/src/main/java/com/example/aramsflashcardapp/MainActivity.java
+++ app/src/main/java/com/example/aramsflashcardapp/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.aramsflashcardapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,16 +15,60 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    TextView flashcardQuestion = findViewById(R.id.flashcard_question);
-    TextView flashcardAnswer = findViewById(R.id.flashcard_answer);
-    flashcardQuestion.setOnClickListener(new View.OnClickListener()
+        TextView flashcardAnswer1 = findViewById(R.id.flashcard_answer1);
+        TextView flashcardAnswer2 = findViewById(R.id.flashcard_answer2);
+        TextView flashcardAnswer3 = findViewById(R.id.flashcard_answer3);
+        ImageView eyeball = findViewById(R.id.toggle_choices_visibility1);
+        ImageView closedeyeball = findViewById(R.id.toggle_choices_visibility2);
 
-    {
+    //If choice is Barrack Obama make the button green
+    flashcardAnswer1.setOnClickListener(new View.OnClickListener() {
         @Override
-        public void onClick (View v){
-            flashcardQuestion.setVisibility(View.INVISIBLE);
-            flashcardAnswer.setVisibility(View.VISIBLE);
+        public void onClick (View v) {
+            findViewById(R.id.flashcard_answer1).setBackgroundColor(getResources().getColor(android.R.color.holo_green_dark, null));
             }
         });
+
+    //If choice is Bill Clinton make Barrack Obama Green and Bill Clinton Red
+    flashcardAnswer2.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick (View v) {
+            findViewById(R.id.flashcard_answer2).setBackgroundColor(getResources().getColor(android.R.color.holo_red_dark, null));
+            findViewById(R.id.flashcard_answer1).setBackgroundColor(getResources().getColor(android.R.color.holo_green_dark, null));
+            }
+        });
+
+    //If choice is George W. Bush make Barrack Obama Green and Bill Clinton Red
+    flashcardAnswer3.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick (View v) {
+        findViewById(R.id.flashcard_answer3).setBackgroundColor(getResources().getColor(android.R.color.holo_red_dark, null));
+        findViewById(R.id.flashcard_answer1).setBackgroundColor(getResources().getColor(android.R.color.holo_green_dark, null));
+        }
+    });
+
+    eyeball.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick (View v) {
+        eyeball.setVisibility(View.INVISIBLE);
+        flashcardAnswer1.setVisibility(View.INVISIBLE);
+        flashcardAnswer2.setVisibility(View.INVISIBLE);
+        flashcardAnswer3.setVisibility(View.INVISIBLE);
+        closedeyeball.setVisibility(View.VISIBLE);
+        }
+    });
+
+    closedeyeball.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick (View v) {
+        closedeyeball.setVisibility(View.INVISIBLE);
+        flashcardAnswer1.setVisibility(View.VISIBLE);
+        flashcardAnswer2.setVisibility(View.VISIBLE);
+        flashcardAnswer3.setVisibility(View.VISIBLE);
+        eyeball.setVisibility(View.VISIBLE);
+        }
+    });
+
     }
+
 }
